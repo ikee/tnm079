4aa4f20a64f038f1f4e0b8c3c2a451c734a3432a
Ikshu Dutta <iksdu821@southfork-12.edu.isy.liu.se>
2015-04-24 15:17:11 +0200

    Lab3 first G

--- Subdivision/UniformCubicSplineSubdivisionCurve.cpp
+++ Subdivision/UniformCubicSplineSubdivisionCurve.cpp
@@ -16,12 +16,41 @@ void UniformCubicSplineSubdivisionCurve::Subdivide()
   // Allocate space for new coefficients
   std::vector<Vector3<float> > newc;
 
-  assert(mCoefficients.size() > 4 && "Need at least 5 points to subdivide");
+  assert(mCoefficients.size() > 4 && "Need at least 5 points to subdivide"); 
+
+  std::cout << "start size of mCoeff is: " << mCoefficients.size() << std::endl; 
 
   // Implement the subdivision scheme for a natural cubic spline here
 
+  // for i = mCoefficients.size:
+  // make 2 new vector
+    // modify vector 1 by c'i
+  //    modify vector 2 by c'i+1/2
+  //  push back v1
+  //  push back v2
+
+  newc.push_back(mCoefficients.front());
+
+  int res_size = (mCoefficients.size()* 2 -1);
+
+  for( int i = 1; i < res_size -1; i++) {
+    Vector3<float> v;
+
+    if( i % 2 ){
+      v = 1/8.0f * (4.0f* mCoefficients.at(i/2) + 4.0f * mCoefficients.at(i/2+1));
+    } else {
+      v = 1/8.0f * (1.0f* mCoefficients.at(i/2-1) + 6.0f * mCoefficients.at(i/2) + 1.0f *  mCoefficients.at(i/2+1));
+    }
+
+    newc.push_back(v);
+  }
+  
+  newc.push_back(mCoefficients.back());
+  
+  std::cerr << "Size: " << newc.size() << " and it should be " << (mCoefficients.size()* 2 -1) << std::endl;  
+
   // If 'mCoefficients' had size N, how large should 'newc' be? Perform a check here!
-  assert(true && "Incorrect number of new coefficients!");
+  assert( (newc.size() == res_size)  && "Incorrect number of new coefficients!");
 
  mCoefficients = newc;
 }
