9c10faabd7f278e2855cf4e42cc84bdf500e3bfa
Ikshu Dutta <iksdu821@southfork-05.edu.isy.liu.se>
2015-09-07 18:32:58 +0200

    Lab3 MVG DONE!

--- Subdivision/StrangeSubdivisionMesh.h
+++ Subdivision/StrangeSubdivisionMesh.h
@@ -15,7 +15,29 @@ protected:
   bool Subdividable(unsigned int fi){
     // Every 4th face is not subdividable - kinda strange!
     // Do something more interesting...
-    return (fi % 4);
+    //
+
+    
+    Vector3<float> pos1, pos2, pos3, facePosition;
+    EdgeIterator eit = GetEdgeIterator( f(fi).edge );
+    pos1 = v(eit.GetEdgeVertexIndex()).pos; eit.Next();
+    pos2 = v(eit.GetEdgeVertexIndex()).pos; eit.Next();
+    pos3 = v(eit.GetEdgeVertexIndex()).pos;
+
+    facePosition = pos1 + pos2;
+    facePosition = facePosition + pos3;
+    facePosition = facePosition/3;
+
+
+    Vector3<float> cam_vec = Vector3<float>(0,0,10) - facePosition;
+    
+    Vector3<float> f_norm = FaceNormal( fi );
+    
+    float result = f_norm * cam_vec.Normalize();
+
+    //result = FaceCurvature(fi);
+    //std::cout << result << std::endl;
+    return (result > 0.0f);
   }
 
 };
